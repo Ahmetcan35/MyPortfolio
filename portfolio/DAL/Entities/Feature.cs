65919c04841428c5fd3d84b01ebfa553fa3065b5
Ahmetcan35 <ahmetcancetinz@gmail.com>
2025-02-25 15:58:38 +0300

    second commit

---
+++ portfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,10 @@
+﻿namespace portfolio.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
