07945e6d47f6d83f5a48fe966147b31dc7127aaa
Ahmetcan35 <ahmetcancetinz@gmail.com>
2025-03-03 03:17:25 +0300

    Statistic ,Messages

---
+++ portfolio/Controllers/InboxController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolio.DAL.Context;
+
+namespace portfolio.Controllers
+{
+    public class InboxController : Controller
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+
+
+        public IActionResult ChangesToIsReadTrue(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult ChangesToIsReadFalse(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult DeleteMessage(int id)
+        {
+            var message = context.Messages.Find(id);
+            context.Messages.Remove(message);
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult MessageDetail(int id)
+        {
+            var value = context.Messages.Find(id);
+            return View("MessageDetail", value);
+        }
+
+    }
+}
---
+++ portfolio/Controllers/StatisticController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolio.DAL.Context;
+
+namespace portfolio.Controllers
+{
+    public class StatisticController : Controller
+    {   PortfolioContext context = new PortfolioContext();
+        public IActionResult Index()
+        {   ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x => x.IsRead== true).Count();
+            ViewBag.v4 = context.Messages.Where(x => x.IsRead == false).Count();
+            return View();
+        }
+    }
+}
